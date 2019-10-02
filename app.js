90ddea2bb176f2e13cfc0cb9e7803611fc0ccbaf
a.abramyants <a.abramyants@fun-box.ru>
2019-10-03 01:37:02 +0300

    Использование body-parser для чтения json из запросов.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
+const jsonParser = bodyParser.json({ type: 'application/json' });
 
 // logging setup
-
 app.use(morgan('short'));
+
+// handle json requests
+app.use(jsonParser);
 
 module.exports = app;
