42a4a2ece9bc19e9318246b0a41cf7b486a15c41
a.abramyants <a.abramyants@fun-box.ru>
2019-10-10 01:15:00 +0300

    Базовая обработка роута /sign-up и создание пользователя.

--- controllers/auth-controller/sign-up.js
+++ controllers/auth-controller/sign-up.js
@@ -1,3 +1,23 @@
-module.exports = function signUp(userService, req, res, next) {
-  //
+module.exports = async function signUp(userService, req, res) {
+  const userCredentials = req.body;
+
+  try {
+    userService.ensureCredentials(userCredentials);
+  } catch (error) {
+    return res.send({ status: 'invalidCredentials', error: error.message });
+  }
+
+  try {
+    const existingUser = await userService.getUserByCredentials(userCredentials);
+
+    if (existingUser) {
+      return res.send({ status: 'userExistsError' });
+    }
+
+    const user = await userService.createUser(userCredentials);
+
+    return res.json({ status: 'ok', user });
+  } catch (e) {
+    return res.send({ status: 'internalError' });
+  }
 };
--- services/user-service.js
+++ services/user-service.js
@@ -2,6 +2,46 @@ class UserService {
   constructor(userModel) {
     this.userModel = userModel;
   }
+
+  ensureCredentials(userCredentials) {
+    const { email, password } = userCredentials;
+    if (!email || !password) {
+      throw new Error('You must provide email and password');
+    }
+  }
+
+  getUserByCredentials(creds) {
+    return this.getUserByEmail(creds.email);
+  }
+
+  getUserByEmail(email) {
+    return new Promise((resolve, reject) => {
+      this.userModel.findOne({ email }, (err, user) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(user);
+      });
+    });
+  }
+
+  createUser(credentials) {
+    const User = this.userModel;
+    const user = new User(credentials);
+
+    return new Promise((resolve, reject) => {
+      user.save((err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(user);
+      });
+    });
+  }
 }
 
 module.exports = UserService;
